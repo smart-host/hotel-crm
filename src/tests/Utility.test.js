d6fa2a5a145237440610d60f5898c73eaddb147f
gnerkus <ifeanyioraelosi@gmail.com>
2020-01-02 12:13:22 +0100

    001: feat: define utility functions

--- src/tests/Utility.test.js
+++ src/tests/Utility.test.js
@@ -13,65 +13,25 @@ const testGuestData = [
   209
 ];
 
-/**
- * The optimization works in the following order
- * 1. Split the guests into two categories:
- * - Cat A pays greater than 100
- * - Cat B pays less than or equal to 100
- * 2. Sort each category according to the value from greatest to least
- * 3. Move top N from Cat A into new array where N is the number of premium rooms
- * 4. Store diff between count of Cat A and number of premium rooms if premium
- * room count is greater as D
- * 5. Move top D from Cat B into premium room array
- * 6. Move to M from Cat B into new array where M is the number of economy rooms
- */
-
- /**
-  * Optimiation functions
-  * 1. Split list of guests into two groups based on supplied criteria
-  * 2. Fill rooms
-  * 3. 
-  */
-
-// const optimize = (guestList, threshold, premiumRoomCount, economyRoomCount) => {
-//   // First function
-//   const sortedGuestList = guestList.sort((a, b) => b - a)
-//   const premiumGuests = sortedGuestList.filter(guest => guest > threshold)
-//   const economyGuests = sortedGuestList.filter(guest => guest <= threshold)
-
-//   // Second function (fills the rooms)
-//   const occupiedPremium = premiumGuests.slice(0, premiumRoomCount);
-
-//   // Third function (fill premium)
-//   const unoccupiedPremiumCount = premiumRoomCount - premiumGuests.length;
-//   if (unoccupiedPremiumCount > 0) {
-//     const economyToPrem = economyGuests.slice(0, unoccupiedPremiumCount)
-//     occupiedPremium = [...occupiedPremium, ...economyToPrem]
-//     economyGuests = economyGuests.slice(unoccupiedPremiumCount)
-//   }
-
-//   const occupiedEconomy = economyGuests.slice(0, economyRoomCount)
-// }
-
 describe('optimization', () => {
   describe('categorizedGuests', () => {
     it('should create a list of premium-paying guests based on criteria', () => {
       expect(categorizeGuests(testGuestData, 100).premiumGuests).toEqual([
-        101,
-        115,
-        155,
+        374,
         209,
-        374
+        155,
+        115,
+        101
       ]);
     });
 
-    it('should create a list of premium-paying guests based on criteria', () => {
+    it('should create a list of economy-paying guests based on criteria', () => {
       expect(categorizeGuests(testGuestData, 100).economyGuests).toEqual([
-        22,
-        23,
-        45,
-        99,
         100,
+        99,
+        45,
+        23,
+        22
       ]);
     });
   });
--- src/utility/index.js
+++ src/utility/index.js
@@ -1,17 +1,33 @@
-export const categorizeGuests = () => {
+export const categorizeGuests = (guestList, threshold) => {
+  const sortedGuestList = guestList.sort((a, b) => b - a)
+  const premiumGuests = sortedGuestList.filter(guest => guest > threshold)
+  const economyGuests = sortedGuestList.filter(guest => guest <= threshold)
+
   return {
-    premiumGuests: [],
-    economyGuests: []
+    premiumGuests,
+    economyGuests
   };
 }
 
-export const occupyRooms = () => {
-  return [];
+export const occupyRooms = (guestList, roomCount) => {
+  return guestList.slice(0, roomCount)
 }
 
-export const fillPremium = () => {
+export const fillPremium = (
+  premRoomCount,
+  premGuestCount,
+  economyGuests,
+  occupiedPremium
+) => {
+  const unoccupiedPremCount = premRoomCount - premGuestCount;
+  if (unoccupiedPremCount > 0) {
+    const economyToPrem = economyGuests.slice(0, unoccupiedPremCount)
+    occupiedPremium = [...occupiedPremium, ...economyToPrem]
+    economyGuests = economyGuests.slice(unoccupiedPremCount)
+  }
+
   return {
-    occupiedPremium: [],
-    economyGuests: []
+    occupiedPremium,
+    economyGuests
   };
 }
